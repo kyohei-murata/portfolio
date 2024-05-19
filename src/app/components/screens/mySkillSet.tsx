cef5b1ba6f9a5e45e0334e6f78603ac9b2a903c8
kyohei murata <kyohei.murata10@gmail.com>
2024-05-19 21:20:06 +0900

    fine adjustment#2

--- src/app/components/screens/mySkillSet.tsx
+++ src/app/components/screens/mySkillSet.tsx
@@ -6,31 +6,6 @@ export const MySkillSet = (): JSX.Element => {
   const ColorAssets = useColorAssets();
   const CustomBreakpoints = useCustomBreakpoints();
   return (
-    // <Box
-    //   minHeight={'100vh'}
-    //   display={'flex'}
-    //   alignItems={'center'}
-    //   justifyContent={'center'}
-    //   bgcolor={'#f8f8f8'}
-    // >
-    //   <Stack
-    //     display={'flex'}
-    //     alignItems={'center'}
-    //     justifyContent={'center'}
-    //     spacing={6}
-    //   >
-    //     <Typography
-    //       variant={CustomBreakpoints.matchesMdUp ? 'h3' : 'h4'}
-    //       color={ColorAssets.textBlack}
-    //       fontWeight={'Bold'}
-    //       id={'my-skills'}
-    //     >
-    //       My Skill Sets
-    //     </Typography>
-    //     <Typography>近日追加！</Typography>
-    //   </Stack>
-    // </Box>
-
     <Box
       minHeight={'100vh'}
       display={'flex'}
@@ -49,7 +24,7 @@ export const MySkillSet = (): JSX.Element => {
           color={ColorAssets.textBlack}
           fontWeight={'Bold'}
         >
-          About This Site
+          My Skill Sets
         </Typography>
         <Typography
           fontSize={CustomBreakpoints.matchesMdUp ? '16px' : '14px'}
